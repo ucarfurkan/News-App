437264e92e72eba320c040a2f05d3febfd9b3c01
ucarfurkan <furkanucar011@gmail.com>
2022-08-12 16:53:50 +0300

    JTable tblUsers connected with database table
    made connection between JTable and a table named user in newsapp database.

--- src/com/newsapp/Model/User.java
+++ src/com/newsapp/Model/User.java
@@ -1,5 +1,12 @@
 package com.newsapp.Model;
 
+import com.newsapp.Helper.DatabaseConnector;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class User {
     private int id;
     private String name;
@@ -56,5 +63,27 @@ public class User {
 
     public void setUserType(String userType) {
         this.userType = userType;
+    }
+
+    public static ArrayList<User> getList(){
+        String query = "SELECT * FROM public.user";
+        ArrayList<User> list = new ArrayList<>();
+        User obj;
+        try {
+            Statement st = DatabaseConnector.getInstance().createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while(rs.next()){
+                obj = new User();
+                obj.setId(rs.getInt("id"));
+                obj.setName(rs.getString("name"));
+                obj.setUserName(rs.getString("user_name"));
+                obj.setPassword(rs.getString("pass"));
+                obj.setUserType(rs.getString("user_type"));
+                list.add(obj);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
--- src/com/newsapp/View/AdminGUI.java
+++ src/com/newsapp/View/AdminGUI.java
@@ -2,8 +2,10 @@ package com.newsapp.View;
 
 import com.newsapp.Helper.Helper;
 import com.newsapp.Model.Admin;
+import com.newsapp.Model.User;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class AdminGUI extends JFrame {
 
@@ -22,6 +24,10 @@ public class AdminGUI extends JFrame {
     private JLabel lblUserUType;
     private JComboBox comboBox1;
     private JButton btnUserAdd;
+    private DefaultTableModel mdlUserList;
+    private Object[] rowUserList;
+
+
     private final Admin admin;
 
     public AdminGUI(Admin admin){
@@ -37,12 +43,44 @@ public class AdminGUI extends JFrame {
         setVisible(true);
 
         lblWelcome.setText("Welcome, " + admin.getName());
+
+        // ModelUserList
+        mdlUserList = new DefaultTableModel(){
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                if(column == 0){
+                    return false;
+                }
+                return super.isCellEditable(row, column);
+            }
+        };
+
+        Object[] colUserList = {"ID","Name","Username","Password","User Type"};
+        mdlUserList.setColumnIdentifiers(colUserList);
+        rowUserList = new Object[colUserList.length];
+        loadUserModel();
+        tblUsers.setModel(mdlUserList);
+        tblUsers.getTableHeader().setReorderingAllowed(false);
     }
 
+    public void loadUserModel(){
+        DefaultTableModel clearModel = (DefaultTableModel) tblUsers.getModel();
+        clearModel.setRowCount(0);
 
-
+        for(User user: User.getList()){
+            rowUserList[0] = user.getId();
+            rowUserList[1] = user.getName();
+            rowUserList[2] = user.getUserName();
+            rowUserList[3] = user.getPassword();
+            rowUserList[4] = user.getUserType();
+            mdlUserList.addRow(rowUserList);
+        }
+    }
 
     public static void main(String[] args) {
-
+        Admin ad = new Admin();
+        AdminGUI adminGUI = new AdminGUI(ad);
     }
+
+
 }
